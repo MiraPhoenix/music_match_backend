b31b368ea8b91315797d1952cf398c11ddab78fd
MiraPhoenix <marisha.k06@gmail.com>
2024-05-25 16:17:32 +0300

    логика 2

--- app/api/handlers.py
+++ app/api/handlers.py
@@ -1,10 +1,28 @@
 from typing import List, Annotated, Union
 
-from fastapi import APIRouter, HTTPException, Header
+from fastapi import APIRouter, HTTPException, Header, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, EmailStr
 import database.db as db
 import bcrypt
 import jwt
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 jwtSecret = "jI1i*2ndIU2j3"
 
@@ -60,7 +78,11 @@ class UpdateUserRequest(BaseModel):
 
 @router.put('/user')
 async def update_user(req: UpdateUserRequest):
-    return {}
+    if id not in db:
+        raise HTTPException(status_code=404, detail="User not found")
+    user = db[id]
+    user.update(req.dict())
+    return {"message": "User updated successfully"}
 
 
 class GetUserResponse(BaseModel):
@@ -73,10 +95,24 @@ class GetUserResponse(BaseModel):
 
 @router.get('/user')
 async def get_user(id: int):
-    return GetUserResponse()
+    if id not in db:
+        raise HTTPException(status_code=404, detail="User not found")
+    user = db[id]
+    reviews = db.get(id, [])
+    all_marks = sum(review['mark'] for review in reviews) / len(reviews) if reviews else 0
+    return List[GetUserResponse]
+
+class Review(BaseModel):
+    id: int
+    user_id: int
+    music_id: int
+    mark: int
+    text: str
 
 @router.get('/user/reviews')
 async def get_user_reviews(id: int):
+    if id not in db:
+        raise HTTPException(status_code=404, detail="Reviews not found")
     return List[Review]
 
 
@@ -88,16 +124,20 @@ class Music(BaseModel):
     data: str
 
 
-@router.get('/musics')
+@router.get('/musics', response_model=List[Music])
 async def get_musics():
     return List[Music]
 
-@router.get('/music')
+@router.get('/music', response_model=Music)
 async def get_music(id: int):
-    return Music()
+    if id not in db:
+        raise HTTPException(status_code=404, detail="Song is not found")
+    return List[Music]
 
-@router.get('/music/search')
+@router.get('/music/search', response_model=List[Music])
 async def search_music(text: str):
+    if text not in db:
+        raise HTTPException(status_code=404, detail="Song is not found")
     return List[Music]
 
 
@@ -130,6 +170,8 @@ class Review(BaseModel):
     mark: int
 
 
-@router.get('/music/review')
+@router.get('/music/review', response_model=List[Review])
 async def get_reviews(id: int):
+    if id not in db:
+        return []
     return List[Review]
